f2085b6507dc0bc13b09d821d4ee4c299b37df17
Carlos Sanchez <carlos.sanchez@telematel.com>
2018-02-26 09:06:07 +0100

    Introducing Factory Pattern

---
+++ FactoryPatternPizza/PizzaStore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternPizza
+{
+    public class PizzaStore
+    {
+        SimplePizzaFactory _factory;
+
+        public PizzaStore(SimplePizzaFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public Pizza OrderPizza(string type)
+        {
+            Pizza = _factory.CreatePizza(type);
+
+            pizza.Prepare();
+            pizza.Bake();
+            pizza.Cut();
+            pizza.Box();
+
+            return pizza;
+        }
+    }
+}
